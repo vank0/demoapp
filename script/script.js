32af493704d059b09d7c5e5bc50c4ff905299346
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 09:39:52 +0200

    Add service worker

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-console.log('script is working!');
+navigator.serviceWorker.register('sw.js');
+Notification.requestPermission(function(result) {
+  if (result === 'granted') {
+    navigator.serviceWorker.ready.then(function(registration) {
+      registration.showNotification('Notification with ServiceWorker');
+    });
+  }
+});
