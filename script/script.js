7b515fd1f90679f96bf324ade3d26543d4f6331e
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 10:17:28 +0200

    sw

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ Notification.requestPermission(function(result) {
     navigator.serviceWorker.ready.then(function(registration) {
       registration.showNotification("Ново харесване в Shell", {
         body: "Вече сте на 5то място!",
-        icon: '../demoapp/img/icon-192x192.png',
+        icon: '../demoapp/img/manifest/icon-192x192.png',
         tag: 'Shell Corrosion Busters',
         image: '../demoapp/img/image.jpg'
       });
