aad9fa989dd96af1acf6cd824ae09d502df18581
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 11:34:43 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,32 +1,28 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
+    console.log('Received a push message', event);
 
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = 'img/manifest/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = 'img/manifest/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
 
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
-  // See: http://crbug.com/463146
-  if (event.action === 'yes') {
-    window.open("http://shell.com");
-} else if (event.action === 'no') {
-    window.open("http://maps.google.com");
-  }
-  event.notification.close();
-
-  });
+    console.log('On notification click: ', event.notification.tag);
+    if (event.action === 'yes') {
+        window.open("http://shell.com");
+    } else if (event.action === 'no') {
+        window.open("http://maps.google.com");
+    }
+    event.notification.close();
 });
