788b6bd10fb87b48b6de7105e7a56a5ed8979b31
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 13:17:37 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
 'use strict';
 
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/styles/styles.css',
+    '/script/script.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
