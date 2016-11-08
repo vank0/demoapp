5d711c9d765238265c7306e869064093e1225916
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 11:38:41 +0200

    sw

--- sw.js
+++ sw.js
@@ -23,19 +23,4 @@ self.addEventListener('notificationclick', function(event) {
     // See: http://crbug.com/463146
     event.notification.close();
 
-    // This looks to see if the current is already open and
-    // focuses if it is
-    event.waitUntil(clients.matchAll({
-        type: 'window'
-    }).then(function(clientList) {
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url === '/' && 'focus' in client) {
-                return client.focus();
-            }
-        }
-        if (clients.openWindow) {
-            return clients.openWindow('/demo');
-        }
-    }));
 });
