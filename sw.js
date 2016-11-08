95ee0c3925c96f49189a3f69c055efedeab42fe0
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 11:33:33 +0200

    sw

--- sw.js
+++ sw.js
@@ -21,21 +21,12 @@ self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
   // Android doesn’t close the notification when you click on it
   // See: http://crbug.com/463146
+  if (event.action === 'yes') {
+    window.open("http://shell.com");
+} else if (event.action === 'no') {
+    window.open("http://maps.google.com");
+  }
   event.notification.close();
 
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: 'window'
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === '/' && 'focus' in client) {
-        return client.focus();
-      }
-    }
-    if (clients.openWindow) {
-      return clients.openWindow('/demoapp');
-    }
-  }));
+  });
 });
