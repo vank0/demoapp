e08824ad0c6300fe7a0f3927e4a491b5eba5c961
Ivan Ivanov <i.ivanov@code-nest.com>
2016-11-08 11:42:54 +0200

    sw

--- sw.js
+++ sw.js
@@ -19,8 +19,6 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
     console.log('On notification click: ', event.notification.tag);
-    // Android doesn’t close the notification when you click on it
-    // See: http://crbug.com/463146
-    event.notification.close();
+    // event.notification.close();
 
 });
